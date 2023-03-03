26cf46a38b37abe1e8a192d006b0095637892869
CherniukAnna <chernukanna@gmail.com>
2023-03-03 12:04:29 +0200

    fifed loader

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -5,7 +5,8 @@ export const Loader = () => {
   return (
     <div className="Loader">
       <RotatingLines
-        strokeColor="#3f51b590"
+        strokeColor="rgba(255, 192, 203, 0.824)"
+        // strokeColor="#3f51b590"
         strokeWidth="5"
         animationDuration="0.75"
         width="96"
